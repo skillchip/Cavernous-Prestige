5d9c5ac290bb97600599df4c1f894269960f52d3
skillchip <121058167+skillchip@users.noreply.github.com>
2023-03-09 14:12:09 -0700

    Update main.js

--- incremental/CavernousII/dist/script/main.js
+++ incremental/CavernousII/dist/script/main.js
@@ -467,15 +467,15 @@ function load() {
         prestige[6].level = saveGame.prestigeArray.value6;   
     }
     // Fix Prestige Values for Hover
-    document.querySelector("prestige#") = writeNumber(prestigecount));
-    document.querySelector("prestigeval0") = writeNumber(prestige[0].level));
-    document.querySelector("prestigeval1") = writeNumber(0.1*prestige[1].level));
+    document.querySelector("prestige#") = writeNumber(prestigecount);
+    document.querySelector("prestigeval0") = writeNumber(prestige[0].level);
+    document.querySelector("prestigeval1") = writeNumber(0.1*prestige[1].level);
     document.querySelector("prestigeval2") = writeNumber(0.95 ** (prestige[2].level ** 0.75));
-    document.querySelector("prestigeval3") = writeNumber(0.1*prestige[3].level));
-    document.querySelector("prestigeval4") = writeNumber(0.1*prestige[4].level));
-    document.querySelector("prestigeval5a") = writeNumber(1+prestige[5].level));
-    document.querySelector("prestigeval5b") = writeNumber(20*prestige[5].level));
-    document.querySelector("prestigeval6") = writeNumber(prestige[6].level));
+    document.querySelector("prestigeval3") = writeNumber(0.1*prestige[3].level);
+    document.querySelector("prestigeval4") = writeNumber(0.1*prestige[4].level);
+    document.querySelector("prestigeval5a") = writeNumber(1+prestige[5].level);
+    document.querySelector("prestigeval5b") = writeNumber(20*prestige[5].level);
+    document.querySelector("prestigeval6") = writeNumber(prestige[6].level);
     
     loadSettings(saveGame.settings);
     zones[0].queues[0].selected = true;
