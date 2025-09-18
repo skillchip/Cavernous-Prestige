cfcc8d7a4b35c42d3046384155ac036b36fffbad
skillchip <121058167+skillchip@users.noreply.github.com>
2025-09-18 12:08:48 -0700

    Catch up to Orig Master

--- incremental/CavernousII/dist/script/main.js
+++ incremental/CavernousII/dist/script/main.js
@@ -194,6 +194,9 @@ function resetLoop(noLoad = false, saveGame = true) {
     if (mana.current > 0) {
         currentLoopLog.finalize();
     }
+    stuff.forEach(s => {
+        s.count = 0;
+    });
     stats.forEach((s, i) => {
         s.reset();
         s.update();
@@ -294,6 +297,7 @@ let save = async function save() {
     const machines = realms.map(r => r.machineCompletions);
     const realmData = realms.map(r => {
         return {
+            maxMult: r.maxMult,
             completed: r.completed,
         };
     });
@@ -401,6 +405,8 @@ function load() {
         recalculateMana();
     }
     saveGame.realmData?.forEach((r, i) => {
+        if (r.maxMult)
+            realms[i].maxMult = r.maxMult;
         if (r.completed)
             realms[i].complete();
     });
